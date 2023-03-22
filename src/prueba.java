91c99495c60042b030d3f88b967303f3864f1d36
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2023-03-22 11:15:18 -0400

    EntidadBD añadidos los cierres de conexión

--- src/prueba.java
+++ src/prueba.java
@@ -1,60 +1,43 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.swing.JOptionPane;
+import org.apache.commons.compress.harmony.pack200.NewAttributeBands.Call;
 
 import Desarrollo.Extras;
 import Servicios_BD.ConnectionManage;
-import Servicios_BD.Persona_BD;
 
-public class prueba {
+public class Prueba {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws Exception {
+		ArrayList<String> listado = new ArrayList<>();
+		listado.add("Rayner");
 		ArrayList<String>listado_municipios_entidades = new ArrayList<String>();
-		String consulta = "SELECT municipio FROM public.entidad Where entidad.centro_trabajo Like ?";
-		PreparedStatement prepa = ConnectionManage.getIntancia().getconection().prepareStatement(consulta);
-		prepa.setString(1,"CUJAE");
-		ResultSet resultado = prepa.executeQuery();
-		while(resultado.next()) {
-			listado_municipios_entidades.add(resultado.getString(1));
-		}
-		JOptionPane.showMessageDialog(null, listado_municipios_entidades.get(0), consulta, JOptionPane.ERROR_MESSAGE);
-	}
-	
-	public static String validar_carnet(String ci_convertir) {
-		String valor = "";
-		if(ci_convertir.length() == 11) {
-			return ci_convertir;
-		}
-		else if(ci_convertir.length() < 11) {
-			int cantidad = 11-ci_convertir.length();
-			if(cantidad == 1) {
-				ci_convertir = "0"+ci_convertir;
-			}
-			else if(cantidad == 2) {
-				ci_convertir = "0"+"0" + ci_convertir;
-			}
-			else if(cantidad >=3) {
-				ci_convertir = "0"+"0"+"0"+ci_convertir;
-			}
-		}
-		else if(ci_convertir.length() >11) {
-			for(int contador = 0; contador<ci_convertir.length();contador++){
-				Character caracter = ci_convertir.charAt(contador);
-				if(caracter.isDigit(caracter) == true) {
-					if(valor == "") {
-						valor = caracter.toString();
+		try (Connection con = ConnectionManage.getIntancia().getconection()){
+			for(int contador = 0; contador< listado.size();contador++) {
+				String consulta = "{?=call entidad_municipio_buscar(?)}";			
+				CallableStatement prepa = con.prepareCall(consulta);
+				prepa.registerOutParameter(1,java.sql.Types.VARCHAR);
+				prepa.setString(2, listado.get(contador));
+				ResultSet resultado = prepa.executeQuery();
+				while(resultado.next()) {
+					boolean verdad = false;
+					String valor = resultado.getString(1);
+					for(int contador2 = 0; contador2<listado_municipios_entidades.size() && verdad == false;contador2++) {
+						if(valor.equals(listado_municipios_entidades.get(contador2)) == true) {
+							verdad = true;
+						}
 					}
-					else {
-						valor = valor + caracter.toString();
+					if(verdad == false) {
+						listado_municipios_entidades.add(valor);
 					}
 				}
-			return valor;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return ci_convertir;
 	}
-		return valor;
-	}
+
 }
